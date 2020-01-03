ddb3dede926a656b1597132b5e3fa8c5d9c4db58
claucookie <claudia.luque.fernandez@gmail.com>
2020-01-03 16:17:54 +0100

    Add missing annotation processor plugin needed for Binding Adapter. Also remove PassesListUiModel giving a try to simpler properties inside the viewmodel.

--- app/src/main/java/dev/claucookielabs/pasbuk/model/PassesListUiModel.kt
+++
@@ -1,7 +0,0 @@
-package dev.claucookielabs.pasbuk.model
-
-sealed class PassesListUiModel {
-    object Loading : PassesListUiModel()
-    object Error : PassesListUiModel()
-    class Content(val passes: List<Passbook>) : PassesListUiModel()
-}
