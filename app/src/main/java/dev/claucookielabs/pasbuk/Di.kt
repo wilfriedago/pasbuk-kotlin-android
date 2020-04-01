d39d2a3bf460e86f3c2c61b4bb6fe3ca112e1db7
claucookie <claudia.luque.fernandez@gmail.com>
2020-04-01 12:37:38 +0100

    Add dependency injection

---
+++ app/src/main/java/dev/claucookielabs/pasbuk/Di.kt
@@ -0,0 +1,19 @@
+package dev.claucookielabs.pasbuk
+
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
+import org.koin.dsl.module
+
+fun App.initKoin() {
+    startKoin {
+        androidLogger(Level.DEBUG)
+        androidContext(this@initKoin)
+        modules(appModule)
+    }
+}
+
+private val appModule = module {
+
+}
